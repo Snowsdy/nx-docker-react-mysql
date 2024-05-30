0b1583cf7a447e6db0064cc5121fdb33c9296533
Snowsdy <shadowss.yt@gmail.com>
2024-05-30 12:50:35 +0200

    🎉 Project started

--- apps/frontend-react/pages/_app.tsx
+++ apps/frontend-react/pages/_app.tsx
@@ -1,18 +1,44 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import './styles.css';
+import React, { useEffect, useState } from 'react';
 
-function CustomApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Welcome to frontend-react!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
-  );
+interface Todo {
+  title: string;
 }
 
-export default CustomApp;
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    fetch('/api/todos')
+      .then((_) => _.json())
+      .then(setTodos);
+  }, []);
+
+  function addTodo() {
+    fetch('/api/addTodo', {
+      method: 'POST',
+      body: '',
+    })
+      .then((_) => _.json())
+      .then((newTodo) => {
+        setTodos([...todos, newTodo]);
+      });
+  }
+
+  return (
+    <>
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((t) => (
+          <li className={'todo'} key={t.title}>
+            {t.title}
+          </li>
+        ))}
+      </ul>
+      <button id={'add-todo'} onClick={addTodo}>
+        Add Todo
+      </button>
+    </>
+  );
+};
+
+export default App;
